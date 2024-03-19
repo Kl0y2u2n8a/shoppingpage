ab11a5be19f2bee75405e11361c3adb2fb8449a3
Kl0y2u2n8a <kevinliang228@gmail.com>
2024-03-19 18:24:21 +0900

    still fixing boxes

--- components/HomeGrid.jsx
+++ components/HomeGrid.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Image from "next/image"
+import accessories from "../public/images/accessories.jpg"
 
 const HomeGrid = () => {
     return (
         <div>
             <div className='grid grid-cols-3 mx-10 gap-2 md:gap-4 text-center'>
-                <div className='shadow-md rounded-lg border mx-auto pt-8 w-20 h-24 sm:w-56 sm:h-32 sm:pt-12 p-auto'>
-                    Accessory
+                <div className='shadow-md rounded-lg mx-auto w-20 h-24 sm:w-56 sm:h-32 p-auto'>
+                    <Image src={accessories} className='z-[-1] object-cover h-full w-full rounded-lg opacity-80' alt='Accessory'/>
+                    <div>
+                        <p className='text-red-500 font-semibold'>Accessory</p>
+                    </div>
                 </div>
 
                 <div className='shadow-md rounded-lg border mx-auto pt-8 w-20 h-24 sm:w-56 sm:h-32 sm:pt-12 p-auto'>
@@ -16,6 +21,9 @@ const HomeGrid = () => {
                     Other
                 </div>
 
+            </div>
+            <div>
+                <p>Hello</p>
             </div>
         </div>
     )
